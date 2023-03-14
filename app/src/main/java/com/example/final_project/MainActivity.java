e03d9b1829e606ef89712a05758524e66c930f4a
ryanw <wang0872@algonquinlive.com>
2023-03-14 10:55:44 -0400

    Added variable binding to gradle Added Button for MarsActivity in MainActivity Added SharedPrefs to Mars

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -2,24 +2,39 @@ package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.example.final_project.databinding.ActivityMainBinding;
+
 import java.io.IOException;
 import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
     Bitmap bitmap;
     ImageView image;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Mars button
+        Button marsButton = binding.marsButton;
+        marsButton.setOnClickListener( marsClk -> {
+            Intent nextPage = new Intent(MainActivity.this, MarsActivity.class);
+            startActivity(nextPage);
+        });
+
+        /*
         image = findViewById(R.id.iMageView);
         new GetImageFromUrl(image).execute(urlKitten);
         EditText widthEditText = findViewById(R.id.widthEditText);
@@ -55,5 +70,6 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(bitmap);
             imageView.setImageBitmap(bitmap);
         }
+        */
     }
 }
