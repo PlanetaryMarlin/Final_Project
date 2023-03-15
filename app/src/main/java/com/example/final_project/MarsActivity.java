9cc89b63e63859088f76d55855c8086667b04a4e
ryanw <wang0872@algonquinlive.com>
2023-03-15 18:53:57 -0400

    temp

--- app/src/main/java/com/example/final_project/MarsActivity.java
+++ app/src/main/java/com/example/final_project/MarsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.final_project.databinding.ActivityMarsBinding;
@@ -20,10 +21,15 @@ public class MarsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         EditText editTextDateNumber = binding.editTextDateNumber;
+        Button searchButton = binding.searchButton;
 
         SharedPreferences prefs = getSharedPreferences("MarsData", Context.MODE_PRIVATE);
         int marsInput = prefs.getInt("marsDate", 0);
         editTextDateNumber.setText(String.valueOf(marsInput));
+
+        searchButton.setOnClickListener( clk -> {
+
+        });
 
     }
 }
