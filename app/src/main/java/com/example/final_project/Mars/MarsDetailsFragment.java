91d6bbff95b552a27d38f4a69268a50e979f0d3e
ryanw <wang0872@algonquinlive.com>
2023-04-10 11:41:51 -0400

    Cleaned up Mars imports

--- app/src/main/java/com/example/final_project/Mars/MarsDetailsFragment.java
+++ app/src/main/java/com/example/final_project/Mars/MarsDetailsFragment.java
@@ -6,13 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.final_project.MarsActivity;
-import com.example.final_project.databinding.ActivityMarsBinding;
 import com.example.final_project.databinding.MarsFragmentBinding;
 import com.google.android.material.snackbar.Snackbar;
 
