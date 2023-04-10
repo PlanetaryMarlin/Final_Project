dab4d9a1a380f43c295178a726868f58e9c93a43
Mahad Mohamed <mahad8574@gmail.com>
2023-04-10 17:18:13 -0400

    Give up

--- app/src/main/java/com/example/final_project/Weather/WeatherView.java
+++ app/src/main/java/com/example/final_project/Weather/WeatherView.java
@@ -6,5 +6,8 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 
 public class WeatherView extends ViewModel {
+    /**
+     * Contain the view
+     */
     public static MutableLiveData<String> weatherView = new MutableLiveData<>();
 }
